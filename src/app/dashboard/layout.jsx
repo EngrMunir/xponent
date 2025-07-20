b0005030080df3f9a5dfbc155685887660076abc
munir <smsirajulmonir@gmail.com>
2025-07-20 10:22:29 +0600

    layout fixed

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,14 +1,26 @@
 import { auth } from "@/auth";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { currentUser } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const DashboardLayout = async ({ children }) => {
   const session = await auth();
-  console.log({ session });
+  const user = await currentUser();
 
   if (!session) redirect("/auth/login");
 
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen">
+        <AppSidebar user={user} />
+        <main className="flex-1 h-screen overflow-y-auto p-6">
+          {children}
+        </main>
+      </div>
+    </SidebarProvider>
+  );
 };
 
 export default DashboardLayout;
